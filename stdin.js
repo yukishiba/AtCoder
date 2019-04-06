cc9c69b2675611f22c2f0edc7b42e234aea8ac70
yukishiba <info@pastelcoder.net>
2019-04-06 20:28:28 +0900

    chore: reset stdin

--- stdin.js
+++ stdin.js
@@ -1,20 +1,5 @@
 'use strict'
 function main(input) {
-  input = removeDangerWord(input.trim())
-  input = input.replace(/eraser?/g, '')
-  input = input.replace(/dream(er)?/g, '')
-  function removeDangerWord (input) {
-    const dangerWord = /dreameraser?/g
-    let wordLength = input.length
-    let newWordLength = 0
-    while (wordLength !== newWordLength) {
-      wordLength = newWordLength
-      input = input.replace(dangerWord, '')
-      newWordLength = input.length
-    }
-    return input
-  }
-
-  console.log(input === '' ? 'YES' : 'NO')
+  console.log(input)
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));

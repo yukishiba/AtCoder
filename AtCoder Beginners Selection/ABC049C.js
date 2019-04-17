863f841f6285baa49dad804af9727b02fe03ba08
yukishiba <info@pastelcoder.net>
2019-04-17 13:12:09 +0900

    fix: ABC049C in biginners selection

--- AtCoder Beginners Selection/ABC049C.js
+++ AtCoder Beginners Selection/ABC049C.js
@@ -2,29 +2,14 @@
 
 'use strict'
 function main(input) {
-  input = removeDangerWord(input.trim())
-  input = input.replace(/eraser?/g, '')
-  input = input.replace(/dream(er)?/g, '')
-  function removeDangerWord (input) {
-    const dangerWord = /dreameraser?/g
-    let wordLength = input.length
-    let newWordLength = 0
-    while (wordLength !== newWordLength) {
-      wordLength = newWordLength
-      input = input.replace(dangerWord, '')
-      newWordLength = input.length
-    }
-    return input
-  }
-
-  console.log(input === '' ? 'YES' : 'NO')
+  console.log(input.trim().replace(/^(dream(er)?|eraser?)*$/, '') === '' ? 'YES' : 'NO')
 }
 
 main('erasedream') // YES
 main('dreameraser') // YES
 main('dreamerer') // NO
 main('dreamereraser') // YES
-main('dreamerdreamerdreamerdreamdreamererasedreamereraserdreamdreamerdreamdreamdreamerasedreamerasererasedreamererasedreameraseeraseeraseerasereraserdreamereraserdreamerasereraseerasereraser')
+main('dreamerdreamerdreamerdreamdreamererasedreamereraserdreamdreamerdreamdreamdreamerasedreamerasererasedreamererasedreameraseeraseeraseerasereraserdreamereraserdreamerasereraseerasereraser') // YES
 
 /*
 dreamer dreamer

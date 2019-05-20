b7046cbdf7b28706aa7bf1f5344b8101da8e938b
yukishiba <info@pastelcoder.net>
2019-05-20 21:10:19 +0900

    fix: ABC126B

--- ABC126/ABC126B.js
+++ ABC126/ABC126B.js
@@ -1,29 +1,31 @@
 // B - YYMM or MMYY 
-// WA
+// FIX 2019/05/20
 'use strict'
 function main(input) {
   input = input.trim()
   const a = Number(input.slice(0, 2))
   const b = Number(input.slice(2, 4))
   const text = ['YYMM','MMYY','AMBIGUOUS','NA']
+  const isMonth = num => ((0 < num) && (num < 13))
+  const am = isMonth(a)
+  const bm = isMonth(b)
   let result = ''
 
-  if ((a === 0) || (b === 0) || ((a > 12) && (b > 12))) {
-    result = text[3]
-  } else if (a < 13) {
-    if (b < 13) {
-      result = text[2]
-    } else {
-      result = text[1]
-    }
-  } else {
+  if (am && bm) {
+    result = text[2]
+  } else if (am) {
+    result = text[1]
+  } else if (bm) {
     result = text[0]
+  } else {
+    result = text[3]
   }
-  console.log(input, a, b, result)
+  console.log(result)
 }
 
 main(`1905`) // YYMM
 main(`9901`) // YYMM
+main(`0001`) // YYMM`
 
 main(`0519`) // MMYY
 main(`0113`) // MMYY
@@ -37,5 +39,4 @@ main(`1700`) // NA
 main(`1799`) // NA
 main(`9999`) // NA
 main(`0000`) // NA
-main(`0001`) // NA
 main(`1313`) // NA

85f1ac93aa895db4dbb9eef14104038af1b8604b
yukishiba <info@pastelcoder.net>
2019-05-28 22:51:59 +0900

    fix: ABC128D

--- ABC128/ABC128D.js
+++ ABC128/ABC128D.js
@@ -1,68 +1,40 @@
 // D - equeue
-// TLE!!!!
+// 解説を読んでの実装
 
 'use strict'
 function main(input) {
   input = input.trim().split('\n')
   const NK = input.shift().split(' ').map(val => Number(val))
+  const count = Math.min(...NK)
   input = input[0].split(' ').map(val => Number(val))
-  let mine = []
 
-  const reduce = arr => {
-    if (arr.length) {
-      return arr.reduce((p, c) => p + c, 0)
-    }
-    return 0
-  }
+  const ans = []
 
-  const choose = (input, mine, K) => {
-    let ans = [reduce(mine)]
-    let i = input.length ? [...input] : []
-    let m = mine.length ? [...mine] : []
-    if (input.length) {
-      ans.push(A(i, m, K))
-      ans.push(B(i, m, K))
+  for(let l = 0; l <= count; l ++) {
+    for (let r = 0; r <= count - l; r ++) {
+      let temp = [
+        ...input.slice(0, l),
+        ...input.slice(NK[0] - r)
+      ].sort((a, b) => a - b)
+      let remain = NK[1] - l - r
+      for (let i = 0; i < remain; i++) {
+        if (temp[i] >= 0) break;
+        temp[i] = 0
+      }
+      ans.push(temp.reduce((p, c) => p + c, 0))
     }
-    if (mine.length) {
-      ans.push(C(i, m, K))
-      ans.push(D(i, m, K))
-    }
-    return Math.max(...ans.filter(val => typeof val === 'number'))
   }
-
-  const A = (input, mine, K) => {
-    let i = input.length ? [...input] : []
-    let m = mine.length ? [...mine] : []
-    m.push(i.shift())
-    m = m.sort((a, b) => a - b)
-    K--
-    return (K > 0) ? choose(i, m, K) : reduce(m)
-  }
-  const B = (input, mine, K) => {
-    let i = input.length ? [...input] : []
-    let m = mine.length ? [...mine] : []
-    m.push(i.pop())
-    m = m.sort((a, b) => a - b)
-    K--
-    return (K > 0) ? choose(i, m, K) : reduce(m)
-  }
-  const C = (input, mine, K) => {
-    let i = input.length ? [...input] : []
-    let m = mine.length ? [...mine] : []
-    i.push(m.shift())
-    K--
-    return (K > 0) ? choose(i, m, K) : reduce(m)
-  }
-  const D = (input, mine, K) => {
-    let i = input.length ? [...input] : []
-    let m = mine.length ? [...mine] : []
-    i.unshift(m.shift())
-    K--
-    return (K > 0) ? choose(i, m, K) : reduce(m)
-  }
-  
-  console.log(choose(input, mine, NK[1]))
+  console.log(Math.max(...ans))
 }
+
+main(`1 1
+-10`) // 0
+
+main(`1 1
+10`) // 10
+
+main(`50 100
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 -25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49`) // 1200
 
 main(`6 4
 -10 8 2 1 2 6`) // 14

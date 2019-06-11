262b4f45c7fc42fa7294593601336fa92f9c87e8
yukishiba <info@pastelcoder.net>
2019-06-11 22:53:49 +0900

    fix: ABC129D

--- ABC129/ABC129D.js
+++ ABC129/ABC129D.js
@@ -1,46 +1,43 @@
 // D - Lamp
-// WA
+
 'use strict'
 function main(input) {
   input = input.trim().split('\n')
   const HW = input.shift().split(' ').map(v => Number(v))
   input = input.map(r => r.split(''))
-  let r = new Array(HW[0]).fill(0)
-  let rMax = new Array(HW[0]).fill([0,0])
-  let c = new Array(HW[1]).fill(0)
-  let cMax = new Array(HW[1]).fill([0,0])
-  
-  input.forEach((row, i) => {
-    row.forEach((col, j) => {
-      if (col === '.') {
-        r[i]++
-        c[j]++
-      } else {
-        if (r[i] > rMax[i][0]) rMax[i] = [r[i], j]
-        if (c[j] > cMax[j][0]) cMax[j] = [c[j], i]
-        r[i] = 0
-        c[j] = 0
-      }
-    })
-  })
-  for (let i = 0; i < HW[0]; i++) {
-    if (r[i] > rMax[i][0]) rMax[i] = [r[i], HW[1]]
-  }
-  for (let j = 0; j < HW[1]; j++) {
-    if (c[j] > cMax[j][0]) cMax[j] = [c[j], HW[0]]
-  }
-  rMax = rMax.map((n, i) => [...n, i]).sort((a, b) => b[0] - a[0])
-  cMax = cMax.map((n, i) => [...n, i]).sort((a, b) => b[0] - a[0])
-
-  for (let i = 0; i < HW[0]; i++) {
-    for (let j = 0; j < HW[1]; j++) {
-      let col = cMax[j][2]
-      if ((col < rMax[i][1]) && (col > rMax[i][1] - rMax[i][0])) {
-        console.log(rMax[i][0] + cMax[j][0] - 1)
-        return
+  let L = []
+  let R = []
+  let U = []
+  let D = []
+  let max = 0
+  for (let h = 0; h < HW[0]; h++) {
+    L[h] = []
+    R[h] = []
+    U[h] = []
+    D[h] = []
+    for (let w = 0; w < HW[1]; w++) {
+      L[h][w] = 0
+      U[h][w] = 0
+      if (input[h][w] === '.') {
+        L[h][w] = w === 0 ? 1 : L[h][w-1] + 1
+        U[h][w] = h === 0 ? 1 : U[h-1][w] + 1
       }
     }
   }
+  for (let h = HW[0] - 1; h >= 0; h--) {
+    for (let w = HW[1] - 1; w >= 0; w--) {
+      R[h][w] = 0
+      D[h][w] = 0
+      if (input[h][w] === '.') {
+        R[h][w] = w === HW[1] - 1 ? 1 : R[h][w+1] + 1
+        D[h][w] = h === HW[0] - 1 ? 1 : D[h+1][w] + 1
+
+        const sum = L[h][w] + R[h][w]+ U[h][w] + D[h][w] - 3
+        if (sum > max) max = sum
+      }
+    }
+  }
+  console.log(max)
 }
 
 main(`4 6
@@ -48,7 +45,6 @@ main(`4 6
 .....#
 ....#.
 #.#...`) // 8
-console.log('---')
 
 main(`8 8
 ..#...#.

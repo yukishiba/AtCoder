2c35747d09bdd1a1d7a9fedd7ed3c8f8015e14a9
yukishiba <info@pastelcoder.net>
2019-06-24 22:16:01 +0900

    fix: abc131_c

--- ABC131/abc131_c.py
+++ ABC131/abc131_c.py
@@ -1,23 +1,14 @@
 ## C - Anti-Division
-## Python で解こうとした。実装例3の誤差が取り切れず。
 
-import math
 import fractions
 
-A, B, C, D = list(map(long,raw_input().split()))
+A, B, C, D = map(int, input().split())
 
 def lcm(x, y):
   return (x * y) // fractions.gcd(x, y)
 
-full = B - B/C - B/D + B/lcm(C,D)
+CD = lcm(C,D)
+A -= 1
+ans = (B - A) - (B//C - A//C) - (B//D - A//D) + (B//CD - A//CD)
 
-print full
-
-hiku = A
-hiku -= math.ceil(float(A)/C)
-hiku -= math.ceil(float(A)/D)
-hiku += math.ceil(float(A)/lcm(C,D))
-
-print float(B)/lcm(C,D)
-
-print '{:.18g}'.format(full - hiku)
+print(ans)

059691a9b3d8a0dd8b230159951cc789461569a8
yukishiba <info@pastelcoder.net>
2019-08-02 12:55:31 +0900

    fix: reset stdin.js

--- stdin.js
+++ stdin.js
@@ -1,12 +1,5 @@
 'use strict'
 function main(input) {
-  input = input.trim().split('\n')
-  const N = Number(input.shift())
-  input = input[0].split(' ').map(n => Number(n))
-  let ans = 0
-  for (let i = 0; i < N; i++) {
-    ans += input[i] - 1
-  }
-  console.log(ans)
+  console.log(input)
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));

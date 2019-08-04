33e21223c39659fd9cbe2046d26775cf1a857a2b
yukishiba <info@pastelcoder.net>
2019-08-04 23:29:16 +0900

    fix: abc136_d

--- ABC136/abc136_d.js
+++ ABC136/abc136_d.js
@@ -1,36 +1,31 @@
 // D - Gathering Children
-// WA でした
+// 参考 https://twitter.com/tempura_cpp/status/1158014535816679424/photo/1
+// だが TLE がでる。。
 'use strict'
 function main(inp) {
-  inp = inp.trim().split('RL')
-  let ans = ''
-  for (let i = 0; i < inp.length - 1; i++) {
-    let a = 1;
-    let b = 1;
-    if (inp[i][inp[i].length - 1] === 'R') {
-      let split = inp[i].split('L')
-      a += split[split.length - 1].length
-    }
-    if (inp[i+1][0] === 'L') {
-      b += inp[i+1].split('R')[0].length
-    }
-    if (Math.max(a, b) % 2 === 0) {
-      let temp = a
-      a = b
-      b = temp
-    }
-    let sum = a + b
-    if (a - b > 1) {
-      a = Math.ceil(sum / 2)
-      b = Math.floor(sum / 2)
-    } else if (b - a > 1) {
-      a = Math.floor(sum / 2)
-      b = Math.ceil(sum / 2)
-    }
-    ans += new Array(inp[i].length).fill(0).join('') + a + b
+  inp = inp.trim()
+  const L = inp.length
+  let next = new Array(L)
+  let ans = new Array(L).fill(0)
+
+  // L/R を具体的な移動先のindexにする。
+  for (let i = 0; i < L; i++) {
+    next[i] = inp[i] === 'R' ? i + 1 : i - 1
   }
-  ans += new Array(inp[inp.length-1].length).fill(0).join('')
-  console.log(ans.split('').join(' '))
+  // 100 回程度でいいのか……
+  for (let j = 0; j < 100; j++) {
+    let newNext = new Array(L)
+    // 移動先の移動先に更新する
+    for (let k = 0; k < L; k++) {
+      newNext[k] = next[next[k]]
+    }
+    next = [...newNext]
+  }
+  // 移動先に選ばれたマスをカウントアップする
+  for (let l = 0; l < L; l++) {
+    ans[next[l]]++
+  }
+  console.log(ans.join(' '))
 }
 
 main(`RRLRL`) // 0 1 2 1 1

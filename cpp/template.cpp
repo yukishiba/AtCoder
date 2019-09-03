342de3d1868555853c9d8d629bb229f05f3d90b5
yukishiba <info@pastelcoder.net>
2019-09-03 13:05:07 +0900

    chore: setting up C++ env.

---
+++ cpp/template.cpp
@@ -0,0 +1,14 @@
+#if 0
+g++ -std=c++14 "$0" && ./a.out
+exit
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+  
+int main() {
+  cout << "Hello, world!" << endl;
+}
+
+// $ sh filename.cpp
+// で実行するためのテンプレートです。

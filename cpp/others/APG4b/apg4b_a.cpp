ac3d8846d73f8162d6751d6c5eb9cdd7d4c7e6a5
yukishiba <info@pastelcoder.net>
2019-09-03 13:05:18 +0900

    add: APG4b

---
+++ cpp/others/APG4b/apg4b_a.cpp
@@ -0,0 +1,11 @@
+#if 0
+g++ -std=c++14 "$0" && ./a.out
+exit
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+}
---
+++ cpp/others/APG4b/apg4b_b.cpp
@@ -0,0 +1,12 @@
+#if 0
+g++ -std=c++14 "$0" && ./a.out
+exit
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+}

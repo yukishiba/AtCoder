b33471ee956d2a8a7d0ed50e7c0c16c48fb6673e
yukishiba <info@pastelcoder.net>
2019-09-27 23:14:11 +0900

    fix: ABC089_c by C++ style

--- cpp/ABC08/9/c.cpp
+++ cpp/ABC08/9/c.cpp
@@ -9,23 +9,23 @@ using namespace std;
 int main() {
   int N;
   cin >> N;
-  long long M[5] = {0,0,0,0,0};
+  vector<long long> M(5, 0);
   long long ans = 0;
 
-  int P[10] = {0,0,0,0,0,0,1,1,1,2};
-  int Q[10] = {1,1,1,2,2,3,2,2,3,3};
-  int R[10] = {2,3,4,3,4,4,3,4,4,4};
+  vector<int> P = {0,0,0,0,0,0,1,1,1,2};
+  vector<int> Q = {1,1,1,2,2,3,2,2,3,3};
+  vector<int> R = {2,3,4,3,4,4,3,4,4,4};
   string S;
   for (int i = 0; i < N; i++) {
     cin >> S;
-    if (S.at(0) == 'M') M[0]++;
-    if (S.at(0) == 'A') M[1]++;
-    if (S.at(0) == 'R') M[2]++;
-    if (S.at(0) == 'C') M[3]++;
-    if (S.at(0) == 'H') M[4]++;
+    if (S.at(0) == 'M') M.at(0)++;
+    if (S.at(0) == 'A') M.at(1)++;
+    if (S.at(0) == 'R') M.at(2)++;
+    if (S.at(0) == 'C') M.at(3)++;
+    if (S.at(0) == 'H') M.at(4)++;
   }
   for (int i = 0; i < 10; i++) {
-    ans += M[P[i]] * M[Q[i]] * M[R[i]];
+    ans += M.at(P.at(i)) * M.at(Q.at(i)) * M.at(R.at(i));
   }
   cout << ans << endl;
 }

fb48e2ef5d84a3d5998de07ad91d4f38cbab5acf
yukishiba <info@pastelcoder.net>
2019-12-29 19:54:15 +0900

    chore: reset stdin.js

--- js/stdin.js
+++ js/stdin.js
@@ -1,20 +1,7 @@
 'use strict'
 
 function main(inp) {
-  inp = inp.trim().split('\n')
-  const N = Number(inp.shift())
-  inp = inp[0].split(' ').map(n => Number(n));
-
-  let count = 0
-  let num = 1
-
-  for (let i = 0; i < N; i++) {
-    if (inp[i] !== num) {
-      count++
-    } else {
-      num++
-    }
-  }
-  console.log(count === N ? -1 : count)
+  inp = inp.trim()
+  console.log(inp)
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
